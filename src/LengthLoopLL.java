00145a87e7ea05db6c03619cee7007e428194935
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-22 21:34:54 -0700

    made a node class and created a method so that i can provide the code for the cycle of the linked list

--- src/LengthLoopLL.java
+++ src/LengthLoopLL.java
@@ -1,5 +1,36 @@
 public class LengthLoopLL {
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    //method for the cycle
+    public static int count(Node head) {
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                int count = 1;
+                slow =slow.next;
+                while (fast != slow) {
+                    count++;
+                    fast = fast.next;
+
+                }return count;
+            }
+        }return 0;
+    }
+
     public static void main(String[] args) {
-        
+
     }
 }
